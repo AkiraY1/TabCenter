373cc0ef91709ae6183de0976662e053a7fc1535
Akira Yoshiyama <luckyanchor2@gmail.com>
2022-07-24 14:13:51 -0700

    searchbar

--- TabCenterApp/views.py
+++ TabCenterApp/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Tournament
+from django.db.models import Q
 
 def home(request):
-    try:
-        tourneys = Tournament.objects.order_by("startDate")
-    except:
-        raise Http404("Error: No tournaments can be found.") #Goes to standard 404 page
+    search_tournament = request.GET.get('search')
+    if search_tournament:
+        tourneys = Tournament.objects.filter(Q(name__icontains=search_tournament))
+    else:
+        try:
+            tourneys = Tournament.objects.order_by("startDate")
+        except:
+            raise Http404("Error: No tournaments can be found.")
     output = {'tourneys': tourneys}
     return render(request, 'TabCenterApp/home.html', output)
 
